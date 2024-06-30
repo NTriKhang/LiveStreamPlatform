277f89f450125c54df12eb83a247e54c73ce65cf
NguyenTriKhang <rokkhangnvd@outlook.com.vn>
2024-06-30 16:53:37 +0700

    modify signup info

--- BackendNet/Dtos/UserSignupDto.cs
+++ BackendNet/Dtos/UserSignupDto.cs
@@ -2,7 +2,10 @@
 {
     public class UserSignupDto
     {
-        public string UserName;
-        public string Password;
+        public string UserName { get; set; }
+        public string Email { get; set; } = "";
+        public string DislayName { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string Role {get;set;} = "";
     }
 }
