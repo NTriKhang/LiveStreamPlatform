77325cf4e7c366f8ae6ae71a0e1b3a28807af0b1
NguyenTriKhang <rokkhangnvd@outlook.com.vn>
2024-10-24 15:17:41 +0700

    add di

--- BackendNet/Program.cs
+++ BackendNet/Program.cs
@@ -99,6 +99,7 @@ internal class Program
         builder.Services.AddScoped<IStatusRepository, StatusRepository>();
         builder.Services.AddScoped<IRecommendRepository, RecommendRepository>();
         builder.Services.AddScoped<ITrendingRepository, TrendingRepository>();
+        builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
         builder.Services.AddScoped<IUserService, UserService>();
         builder.Services.AddScoped<IVideoService, VideoService>();
@@ -113,6 +114,7 @@ internal class Program
         builder.Services.AddScoped<IStatusService, StatusService>();
         builder.Services.AddScoped<IRecommendService, RecommendService>();
         builder.Services.AddScoped<ITrendingService, TrendingService>();
+        builder.Services.AddScoped<ICommentService, CommentService>();
         builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
         .AddCookie(options =>
         {
